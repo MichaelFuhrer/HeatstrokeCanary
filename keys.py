4cf4997f003b8a58db074b0dc425aeedf30b5d51
MichaelFuhrer <57450233+MichaelFuhrer@users.noreply.github.com>
2023-02-20 14:55:04 -0500

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,9 +1,9 @@
 canary_id = '0044644168591603'
-emergency_contact = '+17038357707'
+emergency_contact = '' #Phone number
 
-aws_access_id = 'AKIAUWQ2ACAHMYUDBPLT'
-aws_secret_id = 'ybLqCg1iVse9hXwScy8VZzrPyche/hJgrFLxx5jc'
+aws_access_id = ''
+aws_secret_id = ''
 
-twilio_sid = 'ACa088393c1319c9a37719ba5bc2b1d76a'
-twilio_auth = '9df74eae24ce48ff260742ccbabf0c55'
-twilio_phone = '+12312448840'
+twilio_sid = ''
+twilio_auth = ''
+twilio_phone = ''
